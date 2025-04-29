f0fbfe73e88cb90543a8e68da0caa6c1c6640fc1
haidarniv <haidarapps12@gmail.com>
2025-04-29 11:32:21 +0700

    pembaruan hero dan partners

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,8 @@ const Hero = () => {
       {/* All areas */}
       <section className="relative w-full h-auto overflow-hidden pb-[250px]">
         <video
-          src="https://upscale.id/template/upscale/media/repeat-medium.mp4?v=1"
+          // src="https://upscale.id/template/upscale/media/repeat-medium.mp4?v=1"
+          src="https://videos.pexels.com/video-files/4629800/4629800-uhd_2560_1440_25fps.mp4"
           className="absolute top-0 left-0 w-full h-full object-cover"
           autoPlay
           loop
@@ -24,25 +25,32 @@ const Hero = () => {
         {/* hero section (title etc) */}
         <div className="relative z-30 max-w-[1200px] mx-auto px-[4%] xl:px-0 mt-[200px]">
           <div className="w-4/5 mx-auto flex flex-col items-center text-center text-white gap-[48px]">
-            <h1>Hire Talent On-Demand</h1>
-            <h1>
-              Kami membantu meng-handle semua urusan "talent", agar anda dapat
-              fokus pada pengembangan bisnis
+            <h1 className="">
+              <span className="highlighted-text">Hire Talent On-Demand</span>
+            </h1>
+            <h1 className="">
+              <span className="highlighted-text">
+                Kami membantu meng-handle semua urusan "talent", agar anda dapat
+                fokus pada pengembangan bisnis
+              </span>
             </h1>
             <p className="">
-              Hire software developers, designers, product manager, finance,
-              atau administratif team dari eksklusif network kami. Talent kami
-              bisa bekerja secara remote maupun on-site.
-              <br></br><br />
-              Kontrak fulltime maupun freelance yang disesuaikan dengan
-              kebutuhan bisnis Anda.
+              <span className="highlighted-text">
+                Hire software developers, designers, product manager, finance,
+                atau administratif team dari eksklusif network kami. Talent kami
+                bisa bekerja secara remote maupun on-site.
+                <br></br>
+                <br />
+                Kontrak fulltime maupun freelance yang disesuaikan dengan
+                kebutuhan bisnis Anda.
+              </span>
             </p>
 
             <div className="flex flex-row gap-[40px]">
-              <button className=" bg-[rgb(71,178,228)] rounded-full px-4 py-2 hover:bg-[rgb(48,121,155)]">
+              <button className=" bg-[#446496] rounded-full px-4 py-2 hover:bg-[#6f8db9]">
                 Request Quotation
               </button>
-              <button className="border bg-transparent rounded-full px-4 py-2 hover:bg-[rgb(71,178,228)]">
+              <button className="border bg-transparent rounded-full px-4 py-2 hover:bg-[#6f8db9]">
                 Join As Talent
               </button>
             </div>
--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,4 +1,20 @@
 import Features from "./Features";
+import React from "react";
+
+const mediaPartners = [
+  "https://upscale.id/template/upscale/media/logos/white/logo-1.png",
+  "https://upscale.id/template/upscale/media/logos/white/logo-2.png",
+  "https://upscale.id/template/upscale/media/logos/white/logo-3.png",
+  "https://upscale.id/template/upscale/media/logos/white/logo-4.png",
+  "https://upscale.id/template/upscale/media/logos/white/logo-5.png",
+  "https://upscale.id/template/upscale/media/logos/white/logo-6.png",
+  "https://upscale.id/template/upscale/media/logos/white/logo-6.png",
+  "https://upscale.id/template/upscale/media/logos/white/logo-5.png",
+  "https://upscale.id/template/upscale/media/logos/white/logo-4.png",
+  "https://upscale.id/template/upscale/media/logos/white/logo-3.png",
+  "https://upscale.id/template/upscale/media/logos/white/logo-1.png",
+  "https://upscale.id/template/upscale/media/logos/white/logo-2.png",
+];
 
 const Partners = () => {
   return (
@@ -6,7 +22,17 @@ const Partners = () => {
       <section className="w-full bg-gradient-to-r from-[#A3B8D6] via-[#4F73A8] to-[#37517E]">
         <Features></Features>
         <div className="max-w-[1200px] mx-auto pt-[10px] pb-[80px]">
-          <img src="/partners.png" alt="" />
+          {/* <img src="/partners.png" alt="" /> */}
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-[20px] items-center justify-items-center">
+            {mediaPartners.map((logo, index) => (
+              <img
+                key={index}
+                src={logo}
+                alt=""
+                className="w-[150px] h-auto object-contain"
+              />
+            ))}
+          </div>
         </div>
       </section>
     </>
