4116cb0b6d201cc6f8ef5877034cec7c7445acb4
haidarniv <haidarapps12@gmail.com>
2025-05-24 20:44:51 +0700

    working on layout

---
+++ src/components/layout2.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const layout2 = () => {
+  return (
+    <>
+      <h1>layout2</h1>
+      <p>this is content about layouting</p>
+    </>
+  );
+};
+
+export default layout2;
